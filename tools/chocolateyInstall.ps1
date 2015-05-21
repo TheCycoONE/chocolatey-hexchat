57ed28c6fdcf7a57364894d2e375fb29b4785fc5
Stephen E. Baker <baker.stephen.e@gmail.com>
2015-05-21 08:33:18 -0400

    Import of pjmagee's HexChat package for Chocolatey

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'hexchat'
+$installerType = 'EXE'
+$url = 'https://dl.hexchat.net/hexchat/HexChat%202.10.1%20x86.exe'
+$url64 = 'https://dl.hexchat.net/hexchat/HexChat%202.10.1%20x64.exe'
+$silentArgs = '/verysilent /norestart'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
